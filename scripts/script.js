722802672beaf2c4df6d2b6c43e8f7ba43ff51f1
Rúbem Vieira <rubemvn17@gmail.com>
2023-02-01 22:41:32 -0300

    Atualização de HTML foi adicionado o estilo da section 'Oque faço' foi adicionado arquivos de icones, imagens e arquivos de estilo

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,12 @@
 function redirectToGithub(){
     window.open("https://github.com/Rubemvn");
 }
+
+
+VanillaTilt.init(document.querySelector(".cardiAm"), {
+    max: 25,
+    speed: 400
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".cardiAm"));

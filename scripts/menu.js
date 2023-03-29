b6f7660ff894c5d6a66ebedafa5eec4b7fb19feb
Rúbem Vieira <rubemvn17@gmail.com>
2023-03-28 22:53:55 -0300

    Barra de navegação e menu toggle foram atualizados foi adicionado paginas para os botões de projetos e certificados menu toggle foi atualizado com correções de bugs como o bug que nn permitia scrollar na seção sobre mim, quando acessada pelo menu toggle

--- scripts/menu.js
+++ scripts/menu.js
@@ -10,3 +10,15 @@ menuToggle.addEventListener('click', () => {
     menuSection.classList.toggle("on", show);
     show = !show;
 })
+
+function closeMenuToggle(){
+    
+    let overflow = document.body.style.overflow;
+    
+    if(overflow == "hidden"){
+    
+        document.body.style.overflow = "initial";
+        menuSection.classList.remove("on");
+    
+    }
+}

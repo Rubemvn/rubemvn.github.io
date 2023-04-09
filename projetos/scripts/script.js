9c706cc5e162d5edc6405f838e6f6ac32db4e2fe
Rúbem Vieira <rubemvn17@gmail.com>
2023-04-09 09:50:53 -0300

    Funcionalidade de abrir seção de Projeto atualizada Foi atualizada a funcionalidade de abrir seção individual de cada projeto bugs foram removidos, botões de links estão funcionando, Título de cada Projeto foi corrigido

--- projetos/scripts/script.js
+++ projetos/scripts/script.js
@@ -13,30 +13,42 @@ let description = {
     "mural-de-posts": "Esse Mural de Posts foi desenvolvido em Node.js, Javascript, Bootstrap 5 e HTML. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
 }
 
+let titleProject = {
+    "calculadora": "Calculadora",
+    "jogo-da-memoria": "Jogo da Memoria",
+    "jogo-da-velha": "Jogo da Velha",
+    "portifolio": "Portifólio",
+    "contador": "Contador",
+    "copia-youtube": "Copia do Youtube",
+    "mural-de-posts": "Mural de Posts",
+
+}
 
 function openSection(id) {
 
-    document.body.style.overflow = "hidden";
+    window.
+
+        document.body.style.overflow = "hidden";
     navbar.style.filter = "blur(3px)"
     projects.style.filter = "blur(3px)"
     cover.style.display = "block"
-    
+
 
     createSectionProjects(id)
 }
 
 
-function createSectionProjects(id){
+function createSectionProjects(id) {
     extras.innerHTML = `
                 <div class="extraProjectSection" id="${id}Section">
                     <div class="imagesExtraSection">
                         <img src="./images/previews/${id}.png" alt="">
-                        <button id="btnClose" onclick="closeSection('${id}')">
+                        <button id="btnClose" onclick="closeSection()">
                             <img src="./images/icons/close.png" alt="">
                         </button>
                     </div>
                     <div class="title">
-                        <h3>${id}</h3>
+                        <h3>${titleProject[id]}</h3>
                         <div>
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" />
@@ -52,16 +64,50 @@ function createSectionProjects(id){
                         <button onclick="buttonGithub('${id}')"><img style="width: 30px;" src="./images/icons/github.png" alt=""></button>
                         <button onclick="buttonLink('${id}')"><img style="width: 20px;" src="./images/icons/link.png" alt=""></button>
                     </div>
-                </div>`
+                </div>`;
+
+
+    if(id == "mural-de-posts"){
+
+        let titleDivImg = document.querySelector(".title").lastElementChild;
+        
+        titleDivImg.innerHTML = `
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" />
+            `
+    }
 }
 
-function closeSection(id){
+function closeSection() {
 
-    let idSection = document.querySelector(`#${id}Section`)
-    idSection.remove();
+    let projectSection = document.querySelector(".extraProjectSection")
+
+    projectSection.remove();
 
     document.body.style.overflow = "initial";
     cover.style.display = "none"
     navbar.style.filter = "none"
     projects.style.filter = "none"
 }
+
+function buttonGithub(id) {
+    if (id == "portifolio") {
+        window.open(`https://github.com/Rubemvn/rubemvn.github.io`);
+    } else {
+        window.open(`https://github.com/Rubemvn/${id}`);
+    }
+}
+function buttonLink(id) {
+    if (id == "mural-de-posts") {
+        window.open(`https://github.com/Rubemvn/${id}`);
+    } else {
+        window.open(`https://rubemvn.github.io/${id}`);
+    }
+}
+
+cover.addEventListener("click", () => {
+
+    closeSection()
+})

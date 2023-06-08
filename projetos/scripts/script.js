d42df8a30fce7a4490a975406686e813cdc0e05d
Rúbem Vieira <rubemvn17@gmail.com>
2023-06-07 21:52:57 -0300

    Adicionando novos arquivos foi adicionado um novo projeto(accounts) e um novo certificado

--- projetos/scripts/script.js
+++ projetos/scripts/script.js
@@ -4,40 +4,42 @@ let extras = document.querySelector(".extras")
 let cover = document.querySelector(".cover")
 
 let description = {
-    "calculadora": "Essa calculadora foi desenvolvida em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "jogo-da-memoria": "Esse Jogo da Memória foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "jogo-da-velha": "Esse Jogo da Velha foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "portifolio": "Esse Portifolio foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "contador": "Esse Contador foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "copia-youtube": "Essa Copia do Youtube foi desenvolvida em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
-    "mural-de-posts": "Esse Mural de Posts foi desenvolvido em Node.js, Javascript, Bootstrap 5 e HTML. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "calculadora": "Essa calculadora foi desenvolvida em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "jogo-da-memoria": "Esse Jogo da Memória foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "jogo-da-velha": "Esse Jogo da Velha foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "portifolio": "Esse Portifolio foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "contador": "Esse Contador foi desenvolvido em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "copia-youtube": "Essa Copia do Youtube foi desenvolvida em HTML, CSS, e Javascript. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "mural-de-posts": "Esse Mural de Posts foi desenvolvido em Node.js, Javascript, Bootstrap 5 e HTML. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
+  "accounts": "Esse projeto foi desenvolvido Node.js. <br> Para conhecer mais do projeto, clique nos botões abaixo:",
 }
 
 let titleProject = {
-    "calculadora": "Calculadora",
-    "jogo-da-memoria": "Jogo da Memoria",
-    "jogo-da-velha": "Jogo da Velha",
-    "portifolio": "Portifólio",
-    "contador": "Contador",
-    "copia-youtube": "Copia do Youtube",
-    "mural-de-posts": "Mural de Posts",
+  "calculadora": "Calculadora",
+  "jogo-da-memoria": "Jogo da Memoria",
+  "jogo-da-velha": "Jogo da Velha",
+  "portifolio": "Portifólio",
+  "contador": "Contador",
+  "copia-youtube": "Copia do Youtube",
+  "mural-de-posts": "Mural de Posts",
+  "accounts": "Accounts",
 
 }
 
 function openSection(id) {
 
-    document.body.style.overflow = "hidden";
-    navbar.style.filter = "blur(3px)"
-    projects.style.filter = "blur(3px)"
-    cover.style.display = "block"
+  document.body.style.overflow = "hidden";
+  navbar.style.filter = "blur(3px)"
+  projects.style.filter = "blur(3px)"
+  cover.style.display = "block"
 
 
-    createSectionProjects(id)
+  createSectionProjects(id)
 }
 
 
 function createSectionProjects(id) {
-    extras.innerHTML = `
+  extras.innerHTML = `
                 <div class="extraProjectSection" id="${id}Section">
                     <div class="imagesExtraSection">
                         <img src="./images/previews/${id}.png" alt="">
@@ -62,49 +64,61 @@ function createSectionProjects(id) {
                 </div>`;
 
 
-    if(id == "mural-de-posts"){
+  if (id == "mural-de-posts") {
 
-        let titleDivImg = document.querySelector(".title").lastElementChild;
-        
-        titleDivImg.innerHTML = `
+    let titleDivImg = document.querySelector(".title").lastElementChild;
+
+    titleDivImg.innerHTML = `
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" />
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" />
             `
-    }
+  }
+  if (id == "accounts") {
+
+    let titleDivImg = document.querySelector(".title").lastElementChild;
+
+    titleDivImg.innerHTML = `
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+            `
+  }
 }
 
 function closeSection() {
 
-    let projectSection = document.querySelector(".extraProjectSection")
+  let projectSection = document.querySelector(".extraProjectSection")
 
-    projectSection.remove();
+  projectSection.remove();
 
-    document.body.style.overflow = "initial";
-    cover.style.display = "none"
-    navbar.style.filter = "none"
-    projects.style.filter = "none"
+  document.body.style.overflow = "initial";
+  cover.style.display = "none"
+  navbar.style.filter = "none"
+  projects.style.filter = "none"
 }
 
 function buttonGithub(id) {
-    if (id == "portifolio") {
-        window.open("https://github.com/Rubemvn/rubemvn.github.io");
-    } else {
-        window.open(`https://github.com/Rubemvn/${id}`);
-    }
+  if (id == "portifolio") {
+    window.open("https://github.com/Rubemvn/rubemvn.github.io");
+  } else {
+    window.open(`https://github.com/Rubemvn/${id}`);
+  }
 }
 function buttonLink(id) {
-    if (id == "mural-de-posts") {
-        window.open(`https://github.com/Rubemvn/${id}`);
-    } else if(id == "portifolio"){
-        window.open("https://rubemvn.github.io/");
-    } else {
-        window.open(`https://rubemvn.github.io/${id}`);
-    }
+  if (id == "mural-de-posts") {
+    window.open(`https://github.com/Rubemvn/${id}`);
+  } else if (id == "accounts") {
+    window.open(`https://github.com/Rubemvn/${id}`);
+  } else if (id == "portifolio") {
+    window.open("https://rubemvn.github.io/");
+  } else {
+    window.open(`https://rubemvn.github.io/${id}`);
+  }
 }
 
 cover.addEventListener("click", () => {
 
-    closeSection()
+  closeSection()
 })
